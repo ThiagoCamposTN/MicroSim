8761030cef02e7cf0ce1c35ef7f61998ca45613d
LucasCTN <lucascampostn@gmail.com>
2024-07-25 13:40:02 -0300

    Refatora HexView pra usar botões e acrescenta Inspetor

--- Scripts/HexView.gd
+++ Scripts/HexView.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 var HexViewByte = preload("res://Scenes/HexViewByte.tscn")
-var labels : Array = []
+var elementos_viewer : Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,11 +15,15 @@ func _process(delta):
 	pass
 
 func adicionar_label(texto: String, nome: String = ""):
-	var label = HexViewByte.instantiate()
+	var hex_view_byte : Button = HexViewByte.instantiate()
 	if nome:
-		label.name = nome
-	label.text = texto
-	labels.append(label)
+		hex_view_byte.name = nome
+		hex_view_byte.pressed.connect(ao_clicar_elemento.bind(hex_view_byte))
+	else:
+		hex_view_byte.disabled = true
+		hex_view_byte.focus_mode = FOCUS_NONE
+	hex_view_byte.text = texto
+	elementos_viewer.append(hex_view_byte)
 
 func inicializar_hex_grid():
 	var current_linha = 0
@@ -28,15 +32,16 @@ func inicializar_hex_grid():
 		var endereco_celula = Utils.int_para_hex(current_linha, 3)
 		if current_linha % 16 == 0:
 			adicionar_label(endereco_celula)
-			labels.append(VSeparator.new())
+			elementos_viewer
+			elementos_viewer.append(VSeparator.new())
 		adicionar_label(Utils.int_para_hex(celula, 2), endereco_celula)
 		current_linha += 1
-	
-	for label in labels:
-		$ScrollContainer/HexGrid.add_child(labels.pop_front())
+
+	for label in elementos_viewer:
+		%HexGrid.add_child(elementos_viewer.pop_front())
 
 func atualizar_celula(posicao : int):
-	var celula : Label = $ScrollContainer/HexGrid.get_node(Utils.int_para_hex(posicao, 3))
+	var celula : Button = %HexGrid.get_node(Utils.int_para_hex(posicao, 3))
 	celula.text = Utils.int_para_hex(Memoria.dados[posicao], 2)
 
 func atualizar_grupo_de_celulas(endereco, tamanho):
@@ -44,3 +49,24 @@ func atualizar_grupo_de_celulas(endereco, tamanho):
 	while (i <= endereco + tamanho):
 		atualizar_celula(i)
 		i+=1
+
+func ao_clicar_elemento(elemento: Button):
+	var opcode = "?"
+	if Utils.de_hex_string_para_inteiro(elemento.text) == 0x20:
+		opcode = "LDA (endereçamento imediato)"
+	elif Utils.de_hex_string_para_inteiro(elemento.text) == 0x60:
+		opcode = "LDB (endereçamento imediato)"
+	
+	var text = """
+	Endereço ([color=gray]{end_hex}[/color]):
+		* Hex: [b]{end_hex}[/b]
+		* Dec: [b]{end_dec}[/b]
+		
+	Valor ([color=gray]{valor}[/color]):
+		* Hex: [b]{valor}[/b]
+		* Dec: [b]{decimal}[/b]
+		* Como mnemônico: [b]{opcode}[/b]
+	""".format({"end_hex": elemento.name, "end_dec": Utils.de_hex_string_para_inteiro(elemento.name), "valor": elemento.text,
+		"decimal": Utils.de_hex_string_para_inteiro(elemento.text), "opcode": opcode})
+	
+	get_node("Inspetor/Label").text = text
