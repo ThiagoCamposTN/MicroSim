3bd5fa7c983be12ab83f7988e1aae4f8610b3e3d
LucasCTN <lucascampostn@gmail.com>
2024-07-25 17:54:05 -0300

    Acrescenta contador de linhas e outras funcionalidades ao editor

--- Objects/editor_de_programa.gd
+++ Objects/editor_de_programa.gd
@@ -3,8 +3,13 @@ extends VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	var ch = CodeHighlighter.new()
+	for keyword in ["LDA", "LDB", "STA", "ABA", "CAL", "EXIT"]:
+		ch.add_keyword_color(keyword, Color.SKY_BLUE)
+	ch.add_color_region("{", "}", Color.MEDIUM_PURPLE)
+	ch.number_color = Color.PALE_VIOLET_RED
+	ch.symbol_color = Color.WHITE_SMOKE
+	#$CodeEdit.syntax_highlighter = ch
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,3 +23,4 @@ func _on_button_pressed():
 	SoftwareManager.salvar_codigo_em_memoria(codigo, endereco_inicial)
 	var numero_endereco : int = Utils.de_hex_string_para_inteiro(endereco_inicial)
 	print("Dado no endereço de memória [0000]: ", Memoria.dados[0])
+	
