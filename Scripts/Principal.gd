87053ca31e64c98dcb9d23d745788aac45248ce2
LucasCTN <lucascampostn@gmail.com>
2024-07-30 00:50:23 -0300

    Separa o programa em Memória e Execução com seus panels

--- Scripts/Principal.gd
+++ Scripts/Principal.gd
@@ -3,7 +3,8 @@ extends TabBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SoftwareManager.alterar_caminho_memoria(%MemoriaLineEdit.text)
+	if %MemoriaLineEdit:
+		SoftwareManager.alterar_caminho_memoria(%MemoriaLineEdit.text)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
