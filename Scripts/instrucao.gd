4dffc35129442b04bb4e49c444b975aac42ebe24
ThiagoCamposTN <thiagocampostn@gmail.com>
2025-02-16 00:09:08 -0300

    Encapsula o código de resolução dos parâmetros de uma instrução no local adequado

--- Scripts/instrucao.gd
+++ Scripts/instrucao.gd
@@ -49,6 +49,46 @@ func instrucao_em_string() -> String:
 		_ :
 			return ""
 
+func parametros_em_bytes() -> PackedByteArray:
+	var bytes: PackedByteArray
+
+	if self.parametros and self.parametros[0] == "EXIT":
+		bytes.push_back(0x12)
+		bytes.push_back(0x00)
+		return bytes # salta os parâmetros
+
+	# Resolução dos parâmetros da instrução na memória
+	match self.enderecamento:
+		Instrucao.Enderecamentos.IMEDIATO:
+			var valor = Utils.de_hex_string_para_inteiro(self.parametros[0])
+			bytes.push_back(valor)
+		Instrucao.Enderecamentos.DIRETO:
+			var valor_em_hex 	= Utils.formatar_hex_como_endereco(self.parametros[0])
+			var valor_dividido 	= Utils.de_endereco_hex_para_bytes(valor_em_hex)
+			for valor in valor_dividido:
+				bytes.push_back(valor)
+		Instrucao.Enderecamentos.IMPLICITO:
+			# Não precisa tratar parâmetros
+			pass
+		Instrucao.Enderecamentos.INDEXADO:
+			var valor_em_hex 	= Utils.formatar_hex_como_endereco(self.parametros[0])
+			var valor_dividido 	= Utils.de_endereco_hex_para_bytes(valor_em_hex)
+			for valor in valor_dividido:
+				bytes.push_back(valor)
+		Instrucao.Enderecamentos.INDIRETO:
+			var valor_em_hex 	= Utils.formatar_hex_como_endereco(self.parametros[0])
+			var valor_dividido 	= Utils.de_endereco_hex_para_bytes(valor_em_hex)
+			for valor in valor_dividido:
+				bytes.push_back(valor)
+		Instrucao.Enderecamentos.POS_INDEXADO:
+			# TODO
+			pass
+		Instrucao.Enderecamentos.PRE_INDEXADO:
+			# TODO
+			pass
+	
+	return bytes
+
 static func instrucao_call_exit(instrucao : Instrucao):
 	if not instrucao:
 		return false
