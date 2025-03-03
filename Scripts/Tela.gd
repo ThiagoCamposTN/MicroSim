90b0755a4b589ea28dacb45571df24961c57448f
ThiagoCamposTN <thiagocampostn@gmail.com>
2025-03-03 12:56:32 -0300

    Faz a tela refletir valores reais presentes na memória

--- Scripts/Tela.gd
+++ Scripts/Tela.gd
@@ -1,26 +1,40 @@
-extends Panel
+extends TabBar
 
-@onready var image: Image
+@onready var image	: Image
+@export var largura	: int = 64
+@export var altura	: int = 32
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.image = Image.create(64, 32, false, Image.FORMAT_RGB8)
-	
-	# Define alguns pixels
-	for x in range(self.image.get_width()):
-		for y in range(self.image.get_height()):
-			if x == y:
-				image.set_pixel(x, y, Color(1, 0, 0, 1))  # Linha diagonal vermelha
-			else:
-				image.set_pixel(x, y, Color(0, 0, 0, 1))  # Fundo preto
-	
-	var texture_rect : TextureRect = $TextureRect
-	
+	self.image = Image.create(self.largura, self.altura, false, Image.FORMAT_RGB8)
+
+	var texture_rect : TextureRect = $Panel/TextureRect
 	texture_rect.texture = ImageTexture.create_from_image(image)
 	texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 	texture_rect.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
-	texture_rect.texture.update(image)
+
+	Memoria.memoria_foi_recarregada.connect(self.atualizar_tela)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+func atualizar_tela():
+	var comeco: int = Memoria.celulas.size() - floori(largura * altura / 8.0)
+	var valores = Memoria.celulas.slice(comeco, Memoria.celulas.size())
+
+	var indice: int = 0  # Índice para rastrear a posição na array de bits
+
+	for valor: int in valores:
+		for i: int in range(8): # itera sobre os 8 bits do valor
+			# TODO: provavelmente os bits estão invertidos, talvez precise desinvertê-los
+			var bit = (valor >> i) & 1  # Extrai um bit for vez
+
+			# Converte o índice linear em coordenadas (x, y)
+			var x: int = indice % 64
+			var y: int = floori(indice / 64.0)
+
+			image.set_pixel(x, y, Color(255 & bit, 255 & bit, 255 & bit, 1))
+			indice += 1
+	
+	$Panel/TextureRect.texture.update(self.image)
